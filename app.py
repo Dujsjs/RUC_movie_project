62b66cebfb3b82255537e3726952c001de857f6d
Mengchu Feng <fmccmf666@gmail.com>
2023-12-03 23:06:18 +0800

    完善了界面，基本完成了主要功能

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import joinedload, sessionmaker
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 import os
 import sys
@@ -38,8 +43,10 @@ class Movie_info(movie_db.Model):
     mv_type = movie_db.Column(movie_db.String(10))
     year = movie_db.Column(movie_db.String(10)) #这里要限制输入年份的区间1000~2100，可以在前端限制
     mv_box = movie_db.Column(movie_db.Float) #注意，传入数据后，类型不一致的sqlalchemy会自动修正！
-    online_rate = movie_db.Column(movie_db.Float) #从网站爬取的评分
-    prize_num = movie_db.Column(movie_db.Integer)
+    online_rate = movie_db.Column(movie_db.Float) #从豆瓣获取的评分
+    prize_num = movie_db.Column(movie_db.Integer) #获奖数量
+    comments_num = movie_db.Column(movie_db.Integer) #评论数量
+    duration = movie_db.Column(movie_db.Float) #影片时长
 
 class Actor_info(movie_db.Model):
     act_id = movie_db.Column(movie_db.String(10), nullable = False, primary_key=True)
@@ -115,24 +122,24 @@ def drill_data():
         {'act_id':'2041', 'act_name':'米歇尔·罗德里格兹', 'gender':'女', 'act_country':'美国'}
     ]
     mv_info = [
-        {'mv_id':'1001', 'mv_name':'战狼2', 'rls_date':'07 27 2017 12:00AM', 'mv_country':'中国', 'type':'战争', 'year':'2017', 'box':'56.84', 'rate':'7.1', 'prize_num':'8'},
-        {'mv_id':'1002', 'mv_name':'哪吒之魔童降世', 'rls_date':'07 26 2019 12:00AM', 'mv_country':'中国', 'type':'动画', 'year':'2019', 'box':'50.15', 'rate':'8.4', 'prize_num':'6'},
-        {'mv_id':'1003', 'mv_name':'流浪地球', 'rls_date':'02  5 2019 12:00AM', 'mv_country':'中国', 'type':'科幻', 'year':'2019', 'box':'46.86', 'rate':'7.9', 'prize_num':'5'},
-        {'mv_id':'1004', 'mv_name':'复仇者联盟4', 'rls_date':'04 24 2019 12:00AM', 'mv_country':'美国', 'type':'科幻', 'year':'2019', 'box':'42.5', 'rate':'8.3', 'prize_num':'9'},
-        {'mv_id':'1005', 'mv_name':'红海行动', 'rls_date':'02 16 2018 12:00AM', 'mv_country':'中国', 'type':'战争', 'year':'2018', 'box':'36.5', 'rate':'8.2', 'prize_num':'7'},
-        {'mv_id':'1006', 'mv_name':'唐人街探案2', 'rls_date':'02 16 2018 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2018', 'box':'33.97', 'rate': '6.6', 'prize_num':'7'},
-        {'mv_id':'1007', 'mv_name':'我不是药神', 'rls_date':'07  5 2018 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2018', 'box':'31', 'rate': '9.0', 'prize_num':'12'},
-        {'mv_id':'1008', 'mv_name':'中国机长', 'rls_date':'09 30 2019 12:00AM', 'mv_country':'中国', 'type':'剧情', 'year':'2019', 'box':'29.12', 'rate':'6.6', 'prize_num':'4'},
-        {'mv_id':'1009', 'mv_name':'速度与激情8', 'rls_date':'04 14 2017 12:00AM', 'mv_country':'美国', 'type':'动作', 'year':'2017', 'box':'26.7', 'rate':'7.0', 'prize_num':'5'},
-        {'mv_id':'1010', 'mv_name':'西虹市首富', 'rls_date':'07 27 2018 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2018', 'box':'25.47', 'rate':'6.6', 'prize_num':'5'},
-        {'mv_id':'1011', 'mv_name':'复仇者联盟3', 'rls_date':'05 11 2018 12:00AM', 'mv_country':'美国', 'type':'科幻', 'year':'2018', 'box':'23.9', 'rate':'8.1', 'prize_num':'9'},
-        {'mv_id':'1012', 'mv_name':'捉妖记2', 'rls_date':'02 16 2018 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2018', 'box':'22.37', 'rate':'4.9', 'prize_num':'3'},
-        {'mv_id':'1013', 'mv_name':'八佰', 'rls_date':'08 21 2020 12:00AM', 'mv_country':'中国', 'type':'战争', 'year':'2020', 'box':'30.1', 'rate':'7.5', 'prize_num':'6'},
-        {'mv_id':'1014', 'mv_name':'姜子牙', 'rls_date':'10  1 2020 12:00AM', 'mv_country':'中国', 'type':'动画', 'year':'2020', 'box':'16.02', 'rate':'6.6', 'prize_num':'6'},
-        {'mv_id':'1015', 'mv_name':'我和我的家乡', 'rls_date':'10  1 2020 12:00AM', 'mv_country':'中国', 'type':'剧情', 'year':'2020', 'box':'28.29', 'rate':'7.0', 'prize_num':'7'},
-        {'mv_id':'1016', 'mv_name':'你好，李焕英', 'rls_date':'02 12 2021 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2021', 'box':'54.13', 'rate':'7.7', 'prize_num':'6'},
-        {'mv_id':'1017', 'mv_name':'长津湖', 'rls_date':'09 30 2021 12:00AM', 'mv_country':'中国', 'type':'战争', 'year':'2021', 'box':'53.48', 'rate':'7.4', 'prize_num':'4'},
-        {'mv_id':'1018', 'mv_name':'速度与激情9', 'rls_date':'05 21 2021 12:00AM', 'mv_country':'中国', 'type':'动作', 'year':'2021', 'box':'13.92', 'rate':'5.1', 'prize_num':'5'}
+        {'mv_id':'1001', 'mv_name':'战狼2', 'rls_date':'07 27 2017 12:00AM', 'mv_country':'中国', 'type':'战争', 'year':'2017', 'box':'56.84', 'rate':'7.1', 'prize_num':'8', 'comments_num':'910744', 'duration':'123'},
+        {'mv_id':'1002', 'mv_name':'哪吒之魔童降世', 'rls_date':'07 26 2019 12:00AM', 'mv_country':'中国', 'type':'动画', 'year':'2019', 'box':'50.15', 'rate':'8.4', 'prize_num':'6', 'comments_num':'1837957', 'duration':'110'},
+        {'mv_id':'1003', 'mv_name':'流浪地球', 'rls_date':'02  5 2019 12:00AM', 'mv_country':'中国', 'type':'科幻', 'year':'2019', 'box':'46.86', 'rate':'7.9', 'prize_num':'7', 'comments_num':'1941069', 'duration':'125'},
+        {'mv_id':'1004', 'mv_name':'复仇者联盟4', 'rls_date':'04 24 2019 12:00AM', 'mv_country':'美国', 'type':'科幻', 'year':'2019', 'box':'42.5', 'rate':'8.3', 'prize_num':'9', 'comments_num':'1080448', 'duration':'181'},
+        {'mv_id':'1005', 'mv_name':'红海行动', 'rls_date':'02 16 2018 12:00AM', 'mv_country':'中国', 'type':'战争', 'year':'2018', 'box':'36.5', 'rate':'8.2', 'prize_num':'7', 'comments_num':'1041819', 'duration':'138'},
+        {'mv_id':'1006', 'mv_name':'唐人街探案2', 'rls_date':'02 16 2018 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2018', 'box':'33.97', 'rate': '6.6', 'prize_num':'7', 'comments_num':'908170', 'duration':'121'},
+        {'mv_id':'1007', 'mv_name':'我不是药神', 'rls_date':'07  5 2018 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2018', 'box':'31', 'rate': '9.0', 'prize_num':'12', 'comments_num':'2141621', 'duration':'117'},
+        {'mv_id':'1008', 'mv_name':'中国机长', 'rls_date':'09 30 2019 12:00AM', 'mv_country':'中国', 'type':'剧情', 'year':'2019', 'box':'29.12', 'rate':'6.6', 'prize_num':'4', 'comments_num':'738035', 'duration':'111'},
+        {'mv_id':'1009', 'mv_name':'速度与激情8', 'rls_date':'04 14 2017 12:00AM', 'mv_country':'美国', 'type':'动作', 'year':'2017', 'box':'26.7', 'rate':'7.0', 'prize_num':'5', 'comments_num':'341792', 'duration':'136'},
+        {'mv_id':'1010', 'mv_name':'西虹市首富', 'rls_date':'07 27 2018 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2018', 'box':'25.47', 'rate':'6.6', 'prize_num':'5', 'comments_num':'1024719', 'duration':'118'},
+        {'mv_id':'1011', 'mv_name':'复仇者联盟3', 'rls_date':'05 11 2018 12:00AM', 'mv_country':'美国', 'type':'科幻', 'year':'2018', 'box':'23.9', 'rate':'8.1', 'prize_num':'9', 'comments_num':'782955', 'duration':'149'},
+        {'mv_id':'1012', 'mv_name':'捉妖记2', 'rls_date':'02 16 2018 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2018', 'box':'22.37', 'rate':'4.9', 'prize_num':'3', 'comments_num':'365134', 'duration':'110'},
+        {'mv_id':'1013', 'mv_name':'八佰', 'rls_date':'08 21 2020 12:00AM', 'mv_country':'中国', 'type':'战争', 'year':'2020', 'box':'30.1', 'rate':'7.5', 'prize_num':'6', 'comments_num':'823631', 'duration':'147'},
+        {'mv_id':'1014', 'mv_name':'姜子牙', 'rls_date':'10  1 2020 12:00AM', 'mv_country':'中国', 'type':'动画', 'year':'2020', 'box':'16.02', 'rate':'6.6', 'prize_num':'6', 'comments_num':'608152', 'duration':'110'},
+        {'mv_id':'1015', 'mv_name':'我和我的家乡', 'rls_date':'10  1 2020 12:00AM', 'mv_country':'中国', 'type':'剧情', 'year':'2020', 'box':'28.29', 'rate':'7.0', 'prize_num':'7', 'comments_num':'661982', 'duration':'153'},
+        {'mv_id':'1016', 'mv_name':'你好，李焕英', 'rls_date':'02 12 2021 12:00AM', 'mv_country':'中国', 'type':'喜剧', 'year':'2021', 'box':'54.13', 'rate':'7.7', 'prize_num':'6', 'comments_num':'1429732', 'duration':'128'},
+        {'mv_id':'1017', 'mv_name':'长津湖', 'rls_date':'09 30 2021 12:00AM', 'mv_country':'中国', 'type':'战争', 'year':'2021', 'box':'53.48', 'rate':'7.4', 'prize_num':'4', 'comments_num':'759964', 'duration':'176'},
+        {'mv_id':'1018', 'mv_name':'速度与激情9', 'rls_date':'05 21 2021 12:00AM', 'mv_country':'美国', 'type':'动作', 'year':'2021', 'box':'13.92', 'rate':'5.1', 'prize_num':'5', 'comments_num':'205536', 'duration':'142'}
     ]
     mv_act_relation = [
         {'id':'1', 'mv_id':'1001', 'act_id':'2001', 'relation_type':'主演'},
@@ -194,7 +201,7 @@ def drill_data():
         temp_act = Actor_info(act_id = a['act_id'], act_name = a['act_name'], gender = a['gender'], act_country = a['act_country'])
         movie_db.session.add(temp_act)
     for m in mv_info:
-        temp_mv = Movie_info(mv_id = m['mv_id'], mv_name = m['mv_name'], rls_date = m['rls_date'], mv_country = m['mv_country'], mv_type = m['type'], year = m['year'], mv_box = m['box'], online_rate = m['rate'], prize_num = m['prize_num'])
+        temp_mv = Movie_info(mv_id = m['mv_id'], mv_name = m['mv_name'], rls_date = m['rls_date'], mv_country = m['mv_country'], mv_type = m['type'], year = m['year'], mv_box = m['box'], online_rate = m['rate'], prize_num = m['prize_num'], comments_num = m['comments_num'], duration = m['duration'])
         movie_db.session.add(temp_mv)
     for r in mv_act_relation:
         temp_relation = Mv_Act(id = r['id'], relation_type = r['relation_type'], act_id = r['act_id'], mv_id = r['mv_id'])
@@ -252,16 +259,16 @@ def search():
             mv_type_box_list = type_box.values.tolist()
             doughnut_data = {'labels': mv_type_list, 'data': mv_type_box_list}
 
-            radar_labels = ['电影票房排名', '网络评分排名', '获奖数量排名']
+            radar_labels = ['电影票房排名', '网络评分排名', '获奖数量排名', '评论数量排名', '影片时长排名']
             df_search_rst_ranked = df_search_rst
-            columns_to_rank = ['mv_box', 'online_rate', 'prize_num']
+            columns_to_rank = ['mv_box', 'online_rate', 'prize_num', 'comments_num', 'duration']
             for col in columns_to_rank: # 对每列进行排名并将排名存储到新的列
                 rank_col = f'{col}_Rank'
                 df_search_rst_ranked[rank_col] =  df_search_rst_ranked[col].rank(ascending=False)
 
             radar_data_sets = []
             for index, row in df_search_rst_ranked.iterrows(): #按行循环
-                temp_list = [row['mv_box_Rank'], row['online_rate_Rank'], row['prize_num_Rank']]
+                temp_list = [row['mv_box_Rank'], row['online_rate_Rank'], row['prize_num_Rank'], row['comments_num_Rank'], row['duration_Rank']]
                 temp_dic = {'label': row['mv_name'], 'data': temp_list}
                 radar_data_sets.append(temp_dic)
             radar_data = {'labels': radar_labels, 'datasets': radar_data_sets}
@@ -320,5 +327,67 @@ def demo():
         return render_template('admin_page.html', rst_movies = search_rst)
 
     
-#@app.route('/userpage/<string: username>', methods = ['GET', 'POST'])
-#def import_data()
+@app.route('/box_predict', methods = ['GET', 'POST'])
+def predict():
+    duration = float(request.form['duration'])
+    comments_num = int(request.form['comments_num'])
+    prize_num = int(request.form['prize_num'])
+    online_rate = float(request.form['online_rate'])
+
+    temp_query = Movie_info.query
+    engine = create_engine('sqlite:///data.db') #链接数据库引擎
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    train_dataset = pd.read_sql(temp_query.statement, session.bind) #将查询结果转化为数据框
+
+    t_box_data = train_dataset['mv_box'].to_list()
+    t_duration = train_dataset['duration'].to_list()
+    t_com_num = train_dataset['comments_num'].to_list()
+    t_prz_num = train_dataset['prize_num'].to_list()
+    t_onl_rate = train_dataset['online_rate'].to_list()
+    x_train = np.column_stack((t_duration, t_com_num, t_prz_num, t_onl_rate))
+    y_train = np.array(t_box_data)
+
+    model = LinearRegression()
+    model.fit(x_train, y_train)
+    prediction = model.predict([[duration, comments_num, prize_num, online_rate]])
+    
+    plt.clf() #清除先前已经绘画过的图像
+    plt.scatter(range(len(y_train)), y_train, color='blue', label='historical_box')
+    plt.scatter(len(y_train) + 1, prediction, color='red', marker='o', label='predicted_box')
+    plt.xlabel('movie_number')
+    plt.ylabel('box')
+    plt.legend()
+
+    img = BytesIO()   #将图表保存为字节流
+    plt.savefig(img, format='png')
+    img.seek(0)
+    img_base64 = base64.b64encode(img.getvalue()).decode('utf-8')
+    img_url = f'data:image/png;base64,{img_base64}'
+
+    search_rst = temp_query.all()
+
+    mv_name_list = train_dataset['mv_name'].to_list()
+    mv_box_list = list(map(float, train_dataset['mv_box'].to_list()))
+    line_data = {'labels': mv_name_list, 'data': mv_box_list}
+
+    type_box = train_dataset.groupby('mv_type')['mv_box'].sum()
+    mv_type_list = type_box.index.tolist()
+    mv_type_box_list = type_box.values.tolist()
+    doughnut_data = {'labels': mv_type_list, 'data': mv_type_box_list}
+
+    radar_labels = ['电影票房排名', '网络评分排名', '获奖数量排名', '评论数量排名', '影片时长排名']
+    train_dataset_ranked = train_dataset
+    columns_to_rank = ['mv_box', 'online_rate', 'prize_num', 'comments_num', 'duration']
+    for col in columns_to_rank: # 对每列进行排名并将排名存储到新的列
+        rank_col = f'{col}_Rank'
+        train_dataset_ranked[rank_col] =  train_dataset_ranked[col].rank(ascending=False)
+
+    radar_data_sets = []
+    for index, row in train_dataset_ranked.iterrows(): #按行循环
+        temp_list = [row['mv_box_Rank'], row['online_rate_Rank'], row['prize_num_Rank'], row['comments_num_Rank'], row['duration_Rank']]
+        temp_dic = {'label': row['mv_name'], 'data': temp_list}
+        radar_data_sets.append(temp_dic)
+    radar_data = {'labels': radar_labels, 'datasets': radar_data_sets}
+
+    return render_template('search_mv.html', rst_movies = search_rst, mv_line_data = line_data, mv_doughnut_data = doughnut_data, mv_radar_data = radar_data, prediction=prediction[0], img_url=img_url)
